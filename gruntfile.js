28b2586c30ec8cea7fb56f7debf173a11a5f228c
Braulio Diez Botella <braulio.diez@gmail.com>
2014-05-31 10:50:20 +0200

    min step one

--- gruntfile.js
+++ gruntfile.js
@@ -5,12 +5,26 @@ module.exports = function(grunt) {
 			all: {
 				src: ['./build/**/*.*']
 			}
-		}		
+		},
+
+		ngmin: {
+			all: {
+				files: [
+					{												
+						src: ['./src/**/*.js'],
+						dest: './build/ngvalidationsummary.js',
+						ext: '.js'
+					}
+				]
+			}
+		}
 	});
 
 	grunt.registerTask('default', ['build']);
 
-	grunt.registerTask('build', ['clean']);
+	grunt.registerTask('build', ['clean', 'ngmin']);
 
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-ngmin');
+
 };

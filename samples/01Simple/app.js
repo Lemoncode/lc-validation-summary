fbc0efc8d59ec4bbd718e61b56e483b856519b09
Totemika <totemika@gmail.com>
2014-06-03 12:46:26 +0200

    renames

--- samples/01Simple/app.js
+++ samples/01Simple/app.js
@@ -3,7 +3,7 @@ var sampleApp = angular.module('sampleApp', ['ngValidationSummary'])
 .controller('sampleController', ['$scope', function sampleController($scope){	
 	$scope.model = {
 		nickName:" ",
-		password:" ",
+		password:"",
 		email:" ",
 		phoneNumber:" "
 	}

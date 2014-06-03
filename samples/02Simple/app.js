9a458db039bfbcaac16613693f4e0cd1462b676e
Totemika <totemika@gmail.com>
2014-06-03 21:58:49 +0200

    commit for review

--- samples/02Simple/app.js
+++ samples/02Simple/app.js
@@ -7,42 +7,41 @@ var sampleApp = angular.module('sampleApp', ['ngValidationSummary'])
 		confirmationEmail:""
 	}
 }]);
+sampleApp.directive('checktwofieldsmatch', function () {
 
-// ngValidationSummary.directive('checktwofieldsmatch', function () {
+    return {
+        require: ["ngModel", "^form"],
+        scope: {
+            fieldtomatchmodelproperty: '@',
+            fieldtomatch: '='
+        },
 
-//     return {
-//         require: ["ngModel", "^form"],
-//         scope: {
-//             fieldtomatchmodelproperty: '@',
-//             fieldtomatch: '='
-//         },
+        link: function (scope, elm, attrs, ctrl) {
+            var currentControl = ctrl[0];
+            var currentForm = ctrl[1];
 
-//         link: function (scope, elm, attrs, ctrl) {
-//             var currentControl = ctrl[0];
-//             var currentForm = ctrl[1];
+            var emailMustMatch = function (email, confirmEmail) {
+                return (email === confirmEmail);
+            };
 
-//             var emailMustMatch = function (email, confirmEmail) {
-//                 return (email === confirmEmail);
-//             };
+            currentControl.$parsers.unshift(function (viewValue) {
+                if (typeof (viewValue != "undefined")) {
+                    currentControl.$setValidity('checktwofieldsmatch', emailMustMatch(scope.fieldtomatch, viewValue));
+                }
+                return viewValue;
+            });
 
-//             currentControl.$parsers.unshift(function (viewValue) {
-//                 if (typeof (viewValue != "undefined")) {
-//                     currentControl.$setValidity('checktwofieldsmatch', emailMustMatch(scope.fieldtomatch, viewValue));
-//                 }
-//                 return viewValue;
-//             });
+            var propertyToWatch = scope.fieldtomatchmodelproperty;
+            scope.$parent.$watch(propertyToWatch, function (value, lastValue) {
+                if (typeof (currentControl.$viewValue != "undefined")) {
+                    currentControl.$setValidity('checktwofieldsmatch', emailMustMatch(value, currentControl.$viewValue));
+                }
+            });
+        }
+    };
+});
+sampleApp.config('validationContainerService', function(validationContainerServiceProvider){
 
-//             var propertyToWatch = scope.fieldtomatchmodelproperty;
-//             scope.$parent.$watch(propertyToWatch, function (value, lastValue) {
-//                 if (typeof (currentControl.$viewValue != "undefined")) {
-//                     currentControl.$setValidity('checktwofieldsmatch', emailMustMatch(value, currentControl.$viewValue));
-//                 }
-//             });
-//         }
-//     };
-// })
-// .config('validationContainerServiceProvider', function(validationContainerServiceProvider){
+	validationContainerServiceProvider.addValidation({ type: 'checktwofieldsmatch', friendlyDescription: 'The fields do not match' });
 
-// 	validationContainerServiceProvider.addValidation({ type: 'checktwofieldsmatch', friendlyDescription: 'The fields do not match' });
-
-// });
+});

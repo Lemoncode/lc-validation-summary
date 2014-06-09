90b2535e5557bd0ab614bc49bf2c074f0927222e
Totemika <rosanarufer@gmail.com>
2014-06-09 18:47:59 +0200

    testing validationSummary | help needed

---
+++ test/validationSummarySpec.js
@@ -0,0 +1,37 @@
+describe('validationSummary test', function () {
+
+	var scope;
+	var compile;
+
+	beforeEach(function(){
+
+		module('ngValidationSummary');
+
+		inject(function($compile, $rootScope){
+			scope = $rootScope.$new();
+			compile = $compile;
+		});
+
+	});
+
+	it('', function(){
+		// Arrange
+		var html = 
+		"<div validations-container=''>" +
+		"<form name='personInformation'>"+
+		"<input type='text' id='personName' name='personName' ng-model='person.name'"+
+		"ng-required='true' validationbubble='' friendlyname='Name'/>"+
+		"</form>" +
+		"</div>" +
+		"</div>"+
+		"<div id='validationBox' validationsummary=''></div>";
+		var element = angular.element(html);
+		// Act
+		var compiled = compile(element)(scope);
+		scope.$digest();
+
+		// Assert
+		expect(element[1].find('#validationBox').length).toBe(1);
+	});
+
+});

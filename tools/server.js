b1bb135751d528c7246c3c21939ec7c965100a75
nasdan <idsm00012@gmail.com>
2014-06-25 22:44:40 +0200

    Delete unnecessary file

--- tools/server.js
+++
@@ -1,7 +0,0 @@
-// $ npm install -g connect
-var connect = require('connect');
-connect().use(connect.static("./../")).listen(8080);
-
-// $ node server.js
-// in the browser: http://localhost:8080/src/test.html
-console.log("Open your browser and navigate to...: http://localhost:8080/src/test.html");
